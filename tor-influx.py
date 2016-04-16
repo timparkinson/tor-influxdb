54e5c8d250fc227c9293d8e8c9721ce89d3cf621
tim <tim@tor1.bizarro.org.uk>
2016-04-16 08:24:51 +0100

    Was sending strings, now sending ints

--- tor-influx.py
+++ tor-influx.py
@@ -27,29 +27,29 @@ with Controller.from_socket_file() as controller:
     {
       'measurement': 'total_bytes_read',
       'fields': { 
-        'value': bytes_read
+        'value': int(bytes_read)
       }
     },
     {
       'measurement': 'total_bytes_written',
       'fields': {
-        'value': bytes_written
+        'value': int(bytes_written)
       }
     },
     {
       'measurement': 'bytes_read_1s',
       'fields': {
-        'value': m.group('read')
+        'value': int(m.group('read'))
       }
     },
     {
       'measurement': 'bytes_written_1s',
       'fields': {
-        'value': m.group('write')
+        'value': int(m.group('write'))
       }
     }
     ]
 
 
   client = InfluxDBClient(INFLUXDB_HOST, INFLUXDB_PORT, INFLUXDB_USER, INFLUXDB_PASSWORD, INFLUXDB_DATABASE)
-  client.write_points(points)
+  client.write_points(points, tags=tags)
